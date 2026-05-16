8e1e7a93914b2ae560fe7153183f87b5234ddd3e
goutham-sdet <gouthamg631@gmail.com>
2026-05-16 18:48:07 +0530

    Day 1 : JDK 17 setup Complete

---
+++ src/main/java/Day1SetupCheck.java
@@ -0,0 +1,7 @@
+public class Day1SetupCheck {
+    public static void main(String[] args)
+    {
+        System.out.println("Java Version : " + System.getProperty("java.version"));
+        System.out.println("SDET Day 1 : JDK 17 + IntelliJ + GitHub ready");
+    }
+}
