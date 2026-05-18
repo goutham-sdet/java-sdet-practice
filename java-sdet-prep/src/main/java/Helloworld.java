0be0eb5d286c3d9854a53a3ea922c7f2b6354add
goutham01.t <goutham01.t@lge.com>
2026-05-18 12:55:15 +0530

    First Program in IntelliJ + JDK 17 setup

---
+++ java-sdet-prep/src/main/java/Helloworld.java
@@ -0,0 +1,7 @@
+public class Helloworld {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world  this is my new program with java version " + System.getProperty("java.version"));
+    }
+}
