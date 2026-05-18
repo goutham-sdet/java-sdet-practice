e80a53727d1a971828632de493dc1f4dc1c12f45
goutham01.t <goutham01.t@lge.com>
2026-05-18 17:04:34 +0530

    2 Programs for Day 2

---
+++ java-sdet-prep/src/main/java/Factorial_Recursive.java
@@ -0,0 +1,17 @@
+public class Factorial_Recursive {
+    static int factorial(int n)
+    {
+        if(n==0)
+        {
+            return 1;
+        }
+        return n*factorial(n-1); // a method calling by itself
+    }
+
+    public static void main(String[] args)
+    {
+        int num = 25;
+        int result = factorial(num);
+        System.out.println("The factorial of " + num + " is " + result +  " (Recursive method)");
+    }
+}
