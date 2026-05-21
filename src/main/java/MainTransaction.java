741002ec39f587a01547e516a70f36dfc9e0e4ce
goutham01.t <goutham01.t@lge.com>
2026-05-21 13:46:47 +0530

    consolidate all java files into src/main/java , remove duplicate java-sdet-prep

---
+++ src/main/java/MainTransaction.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MainTransaction {
+    static double amount;
+    public static void main(String[] args)
+    {
+        BankAccount account1 = new BankAccount("1234ABC" , 6000);
+        account1.deposit(5000);
+        account1.withdraw(2000);
+        account1.deposit(1000);
+        System.out.println("The Current available balance after this transaction is : " + account1.getBalance());
+    }
+}
